3e50e9f27375faf0752e769e401e9045c0596ff9
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2020-01-26 22:35:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/laboratorio2/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.laboratorio2
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    var contador = 0
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnLongClickListener{
+            if (contador <=10 ){
+                val aviso = 10 - contador
+                Toast.makeText(this, "Faltan " + aviso.toString()  + " para el objetivo", Toast.LENGTH_SHORT).show()
+
+            }
+            if (contador > 10 ){
+                val aviso = 20 - contador
+                Toast.makeText(this, "Faltan " + aviso.toString()  + " para el objetivo", Toast.LENGTH_SHORT).show()
+
+            }
+
+            true
+        }
+
+
+
+    }
+
+    fun ContarVuelta(view: View) {
+
+        val rollButton1: EditText = findViewById(R.id.editText2)
+        val Imagen: ImageView = findViewById(R.id.imageView)
+        val Imagen2: ImageView = findViewById(R.id.imageView2)
+        if (contador <=20) {
+            contador = contador + 1
+                rollButton1.setText(contador.toString())
+            if(contador == 10){
+                Imagen.visibility = View.VISIBLE
+            }
+            if(contador > 10){
+                Imagen.visibility = View.GONE
+            }
+            if(contador < 10){
+                Imagen.visibility = View.GONE
+            }
+            if(contador == 20){
+                Imagen2.visibility = View.VISIBLE
+            }
+            if(contador < 20){
+                Imagen2.visibility = View.GONE
+            }
+            if(contador > 20){
+                Imagen.visibility = View.GONE
+            }
+
+            }
+        if (contador > 20){
+            contador = 20
+            rollButton1.setText(contador.toString())
+        }
+        }
+
+
+    fun Reiniciar(view: View) {
+        val rollButton1: EditText = findViewById(R.id.editText2)
+        contador = 0
+        rollButton1.setText(contador.toString())
+    }
+
+}
